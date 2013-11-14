58add937d1dfd1982e89a1f74225df820b9bb132
hghowe <hghowe@gmail.com>
2013-11-13 23:12:37 -0600

    Added eyes variable

---
+++ GitTest/src/gittest/Potato.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author harlan.howe
+ */
+public class Potato {
+    private String eyes;
+}
